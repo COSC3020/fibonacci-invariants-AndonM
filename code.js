60aaf826cf907335a0aaa079190a087ac9588878
Andon Mefford <andonmefford@outlook.com>
2023-10-11 21:52:27 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
-function fib(n) {
-    return n;
+function generateFibonacciArray(numbersToGenerate) {
+    if (numbersToGenerate <= 0) {
+        return [];
+    } else if (numbersToGenerate === 1) {
+        return [0];
+    } else if (numbersToGenerate === 2) {
+        return [0, 1];
+    } else {
+        const sequence = generateFibonacciArray(numbersToGenerate - 1);
+        sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+        return sequence;
+    }
 }
