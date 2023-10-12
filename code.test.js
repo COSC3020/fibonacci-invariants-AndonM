d4ded117aef3cc50558bdd33e999af45a4399d0e
Andon Mefford <andonmefford@outlook.com>
2023-10-11 22:38:57 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,21 +1,22 @@
 const fs = require('fs');
 const jsc = require('jsverify');
 
-eval(fs.readFileSync('code.js')+'');
+eval(fs.readFileSync('code.js') + '');
 
 function fibTest(n) {
-    let fib_solns = [0];
-    if(n == 0) return fib_solns;
-    fib_solns[1] = 1;
-    if(n == 1) return fib_solns;
-    for(let i = 2; i <= n; i++) fib_solns[i] =
-        fib_solns[i-1] + fib_solns[i-2];
+    let fib_solns = [];
+    for (let i = 0; i < n; i++) {
+        if (i === 0) fib_solns.push(0);
+        else if (i === 1) fib_solns.push(1);
+        else fib_solns.push(fib_solns[i - 1] + fib_solns[i - 2]);
+    }
     return fib_solns;
 }
 
-const test =
-    jsc.forall("nat", function(n) {
-        return JSON.stringify(generateFibonacciArray(n)) ==
-            JSON.stringify(fibTest(n));
-    });
-jsc.assert(test);
+const test = jsc.forall("nat", function (n) {
+    const result = generateFibonacciArray(n);
+    const expected = fibTest(n);
+    return JSON.stringify(result) === JSON.stringify(expected);
+});
+
+jsc.assert(test)
